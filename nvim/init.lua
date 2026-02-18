286a03013987e94c94918bef090621aee1760004
Matthew Paredes <matthewparedes2k3@gmail.com>
2026-02-17 19:14:00 -0800

    Update nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,135 +1,146 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 
 
 plugins = {
-	{
-		'Shatur/neovim-ayu',
-		opts = {
-			mirage = true,
-		},
-		config = function(PluginSpec, opts)
-			require("ayu").setup(opts)
-			require("lualine").setup({ theme="ayu" }) -- lualine ayu theme
-			require("ayu").colorscheme()
-		end
-	},
-	{
-		'nvim-lualine/lualine.nvim',
-		dependencies = { 'nvim-tree/nvim-web-devicons' }
-	},
-	{
-		"lukas-reineke/indent-blankline.nvim",
-		main = "ibl",
-		opts = {},
-		dependencies = {
-			--"HiPhish/rainbow-delimiters.nvim",
-			"nvim-treesitter/nvim-treesitter",
-		},
-		config = function()
-			local highlight = {
-				--[[
-				"RainbowRed",
-				"RainbowYellow",
-				"RainbowBlue",
-				"RainbowOrange",
-				"RainbowGreen",
-				"RainbowViolet",
-				"RainbowCyan", 
-                ]]--
-				"myColor",
-			}
-			local hooks = require("ibl.hooks")
-			hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
-                --[[
-				vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
-				vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
-				vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
-				vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
-				vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
-				vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
-				vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
-                ]]--
-				vim.api.nvim_set_hl(0, "myColor", { fg = "#555555"})
-			end)
-			--vim.g.rainbow_delimiters = { highlight = highlight }
-			require("ibl").setup { indent = { highlight = highlight } }
-			--hooks.register(hooks.type.SCOPE_HIGHLIGHT, hooks.builtin.scope_highlight_from_extmark)
-		end
-	},
     {
-        "mason-org/mason.nvim",
-        opts = {}
-    },
-	{
-		"williamboman/mason-lspconfig.nvim",
-		event = { "BufReadPre", "BufNewFile" },
-		dependencies = {
-			"hrsh7th/cmp-nvim-lsp",
-			"williamboman/mason.nvim",
-			"hrsh7th/nvim-cmp",
-		},
-		config = function()
-			require("mason").setup()
-			require("mason-lspconfig").setup()
-
-			local lspconfig = require("lspconfig")
-			local cmp = require("cmp")
-			cmp.setup{
-				sources = {
-					{ name = 'nvim_lsp' }
-				}
-			}
-			local cmp_nvim_lsp = require("cmp_nvim_lsp")
-
-			local capabilities = cmp_nvim_lsp.default_capabilities()
-
-			local myservers = {"pyright", "html", "clangd", "lua_ls", "bashls", "cssls", "jdtls", "ts_ls", "eslint", "omnisharp", "dartls"}
-			for _,serv in ipairs(myservers) do
-				lspconfig[serv].setup({
-					capabilities = capabilities,
-				})
-			end
-		end,
-	},
-	{ "neovim/nvim-lspconfig"},
-	{ "elkowar/yuck.vim"},
-	{
-		"norcalli/nvim-colorizer.lua",
-		config = function()
-			require("colorizer").setup({"*";css = {rgb_fn = true;};})
-		end
-	},
-	{
-	    "nvim-neo-tree/neo-tree.nvim",
-	    branch = "v3.x",
-	    dependencies = {
-	      "nvim-lua/plenary.nvim",
-	      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-	      "MunifTanjim/nui.nvim",
-	      -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
-	    }
-	},
-    {
-        'nvim-flutter/flutter-tools.nvim',
-        lazy = false,
-        dependencies = {
-            'nvim-lua/plenary.nvim',
-            'stevearc/dressing.nvim', -- optional for vim.ui.select
+        'Shatur/neovim-ayu',
+        opts = {
+            mirage = true,
         },
-        config = true,
+        config = function(PluginSpec, opts)
+            require("ayu").setup(opts)
+            require("lualine").setup({ theme = "ayu" }) -- lualine ayu theme
+            require("ayu").colorscheme()
+        end
     },
-    {"ellisonleao/glow.nvim", config = true, cmd = "Glow"},
+    {
+        'nvim-lualine/lualine.nvim',
+        dependencies = { 'nvim-tree/nvim-web-devicons' }
+    },
+    {
+        "lukas-reineke/indent-blankline.nvim",
+        main = "ibl",
+        ---@module "ibl"
+        ---@type ibl.config
+        opts = {},
+        config = function()
+            local highlight = {
+                "CursorColumn",
+                "Whitespace",
+            }
+            require("ibl").setup {
+                indent = { highlight = highlight, char = "" },
+                whitespace = {
+                    highlight = highlight,
+                    remove_blankline_trail = false,
+                },
+                scope = { enabled = false },
+            }
+        end
+    },
+    {
+        "mason-org/mason-lspconfig.nvim",
+        opts = {},
+        dependencies = {
+            { "mason-org/mason.nvim", opts = {} },
+            "neovim/nvim-lspconfig",
+        },
+    },
+    { "elkowar/yuck.vim" },
+    {
+        "norcalli/nvim-colorizer.lua",
+        config = function()
+            require("colorizer").setup({ "*", css = { rgb_fn = true, }, })
+        end
+    },
+    {
+        "nvim-neo-tree/neo-tree.nvim",
+        branch = "v3.x",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+            "MunifTanjim/nui.nvim",
+            -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+        }
+    },
+    { "ellisonleao/glow.nvim", config = true, cmd = "Glow" },
+    {
+        'saghen/blink.cmp',
+        -- optional: provides snippets for the snippet source
+        dependencies = { 'rafamadriz/friendly-snippets' },
+
+        -- use a release tag to download pre-built binaries
+        version = '1.*',
+        -- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
+        -- build = 'cargo build --release',
+        -- If you use nix, you can build from source using latest nightly rust with:
+        -- build = 'nix run .#build-plugin',
+
+        ---@module 'blink.cmp'
+        ---@type blink.cmp.Config
+        opts = {
+            -- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
+            -- 'super-tab' for mappings similar to vscode (tab to accept)
+            -- 'enter' for enter to accept
+            -- 'none' for no mappings
+            --
+            -- All presets have the following mappings:
+            -- C-space: Open menu or open docs if already open
+            -- C-n/C-p or Up/Down: Select next/previous item
+            -- C-e: Hide menu
+            -- C-k: Toggle signature help (if signature.enabled = true)
+            --
+            -- See :h blink-cmp-config-keymap for defining your own keymap
+            keymap = { preset = 'super-tab' },
+
+            appearance = {
+                -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+                -- Adjusts spacing to ensure icons are aligned
+                nerd_font_variant = 'mono'
+            },
+
+            -- (Default) Only show the documentation popup when manually triggered
+            completion = { documentation = { auto_show = false } },
+
+            -- Default list of enabled providers defined so that you can extend it
+            -- elsewhere in your config, without redefining it, due to `opts_extend`
+            sources = {
+                default = { 'lsp', 'path', 'snippets', 'buffer' },
+            },
+
+            -- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
+            -- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
+            -- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
+            --
+            -- See the fuzzy documentation for more information
+            fuzzy = { implementation = "prefer_rust_with_warning" }
+        },
+        opts_extend = { "sources.default" }
+    },
+    {
+        'numToStr/Comment.nvim',
+        opts = {
+            -- add any options here
+            toggler = {
+                line = '<C-/>'
+            },
+            opleader = {
+                line = '<C-/>'
+            }
+        }
+    }
+
 }
 
 vim.cmd("set number")
@@ -139,4 +150,13 @@ vim.cmd("set shiftwidth=4")
 vim.cmd("set expandtab")
 vim.cmd("set smartindent")
 vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
-require("lazy").setup(plugins,opts)
+
+
+vim.api.nvim_create_autocmd("BufWritePre", {
+    buffer = buffer,
+    callback = function()
+        vim.lsp.buf.format { async = false }
+    end
+})
+
+require("lazy").setup(plugins, opts)
